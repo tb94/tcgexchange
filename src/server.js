2d2e8b4bb95ea940dbf74e39aea9ca282fcdc367
Tim <timbretz94@gmail.com>
2025-04-01 16:53:06 -0500

    add search endpoint to query for cards

--- src/server.js
+++ src/server.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./routes/users');
+const cardRoutes = require('./routes/cards');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/users', usersRoutes);
+app.use('/cards', cardRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });

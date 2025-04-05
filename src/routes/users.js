877f65a5f73ac90368407947e66f4b14adb5e1b7
Tim <timbretz94@gmail.com>
2025-04-04 23:49:52 -0500

    connect login page to auth endpoint
    serving the page in the users routes, might want to make a route for all static pages instead.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const usersController = require('../controllers/usersController');
 const authMiddleware = require('../middlewares/auth');
+const path = require('path');
 
 const router = express.Router();
 
 router.post('/signup', usersController.signup);
 router.post('/login', usersController.login);
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/login.html'));
+});
+
 router.get('/me', authMiddleware, usersController.getCurrentUser);
 
 module.exports = router;

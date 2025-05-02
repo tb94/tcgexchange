90d0b538318dd84942689a9970637172b25dca9d
Tim <timbretz94@gmail.com>
2025-05-02 13:41:26 -0500

    eslsint Airbnb config
    this doesn't lint the client so don't be bad

--- models/index.js
+++ models/index.js
@@ -1,12 +1,36 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/config.json')[process.env.NODE_ENV || 'development'];
+/* eslint-disable strict */
 
-const sequelize = new Sequelize(config);
+'use strict';
 
-const db = {
-  sequelize,
-  Sequelize,
-  User: require('./user')(sequelize, Sequelize),
-};
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const basename = path.basename(__filename);
+const db = {};
+// eslint-disable-next-line import/no-dynamic-require
+const config = require(`${__dirname}/../../config/database.js`)[env];
+
+// eslint-disable-next-line prefer-const
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();

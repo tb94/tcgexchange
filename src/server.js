0bef897db735642089aec27878978ccddc718be5
Tim <timbretz94@gmail.com>
2025-05-18 12:57:14 -0500

    update static served directory structure and routing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const usersRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
 
@@ -9,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public', 'views')));
 
 app.use('/auth', usersRoutes);
 app.use('/cards', cardRoutes);
